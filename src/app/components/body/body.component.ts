fa076be407a26fb32359c07f8cecf38c0ceccc89
Tim Lanbacher <timlanbacher@gmail.com>
2019-05-22 02:30:55 -0700

    Application working with given plz

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -19,6 +19,7 @@ export class BodyComponent implements OnInit {
       this.meteoSwissData = data;
     })
   }
+
   
 
 }
--- src/app/service/rest.service.ts
+++ src/app/service/rest.service.ts
@@ -11,11 +11,15 @@ export class RestService {
   private weatherUrl: string;
 
   constructor(private http: HttpClient) { 
-    this.weatherUrl = 'https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz=882000'
+    this.weatherUrl = 'https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz='
   }
 
   public findAll(): Observable<MeteoSwissData> {
-    return this.http.get<MeteoSwissData>(this.weatherUrl);
+    return this.http.get<MeteoSwissData>(this.weatherUrl + "882000");
+  }
+
+  public find(plz: string): Observable<MeteoSwissData> {
+    return this.http.get<MeteoSwissData>(this.weatherUrl + plz)
   }
 
 
